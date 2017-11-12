1471f14956b584295be80a44a8c18fa8296ff0ff
eavichay <eavichay@gmail.com>
2017-11-12 12:26:35 +0200

    lint

--- test/parse-with-traverse.test.js
+++ test/parse-with-traverse.test.js
@@ -3,14 +3,18 @@ var fs = require('fs');
 
 var plugin = require('../lib/index');
 var stubProjectLocation = './test/stubs/dummy_project_1/';
-var targetPackagesConfigFile = stubProjectLocation + 'dummy_project_1/packages.config';
-var alternatePackagesFolder = stubProjectLocation + 'alternate_packages';
-var targetCSProjFile = stubProjectLocation + 'dummy_project_1/WebApplication1.csproj';
+var targetPackagesConfigFile =
+  stubProjectLocation + 'dummy_project_1/packages.config';
+var alternatePackagesFolder =
+  stubProjectLocation + 'alternate_packages';
+var targetCSProjFile =
+  stubProjectLocation + 'dummy_project_1/WebApplication1.csproj';
 
 
 test('parse .csproj and traverse packages', function (t) {
   var expectedTreeFile =
-    fs.readFileSync(stubProjectLocation + 'dummy_project_1/expected_csproj.json');
+    fs.readFileSync(
+      stubProjectLocation + 'dummy_project_1/expected_csproj.json');
   var expectedTree = JSON.parse(expectedTreeFile.toString());
 
   plugin.inspect(null, targetCSProjFile, null)
@@ -32,7 +36,8 @@ test('parse .csproj and traverse packages', function (t) {
 
 test('parse packages.config and traverse packages', function (t) {
   var expectedTreeFile =
-    fs.readFileSync(stubProjectLocation + 'dummy_project_1/expected_pkgcfg.json');
+    fs.readFileSync(
+      stubProjectLocation + 'dummy_project_1/expected_pkgcfg.json');
   var expectedTree = JSON.parse(expectedTreeFile.toString());
 
   plugin.inspect(null, targetPackagesConfigFile, null)
@@ -52,26 +57,28 @@ test('parse packages.config and traverse packages', function (t) {
   })
 });
 
-test('parse packages.config and traverse alternate packages folder', function (t) {
-  var expectedTreeFile =
-    fs.readFileSync(stubProjectLocation + 'dummy_project_1/expected_pkgcfg.json');
-  var expectedTree = JSON.parse(expectedTreeFile.toString());
+test('parse packages.config and traverse alternate packages folder',
+  function (t) {
+    var expectedTreeFile =
+      fs.readFileSync(
+        stubProjectLocation + 'dummy_project_1/expected_pkgcfg.json');
+    var expectedTree = JSON.parse(expectedTreeFile.toString());
 
-  plugin.inspect(null, targetPackagesConfigFile, {
-    packagesFolder: alternatePackagesFolder,
-  })
-  .then(function (result) {
-    t.test('traversing', function (t) {
-      t.deepEqual(
-        result.package.dependencies,
-        expectedTree.package.dependencies);
-      t.end();
+    plugin.inspect(null, targetPackagesConfigFile, {
+      packagesFolder: alternatePackagesFolder,
     })
-    return result;
-  })
-  .then(function (result) {
-    if (result) {
-      t.end();
-    }
-  })
-});
+    .then(function (result) {
+      t.test('traversing', function (t) {
+        t.deepEqual(
+          result.package.dependencies,
+          expectedTree.package.dependencies);
+        t.end();
+      })
+      return result;
+    })
+    .then(function (result) {
+      if (result) {
+        t.end();
+      }
+    })
+  });
