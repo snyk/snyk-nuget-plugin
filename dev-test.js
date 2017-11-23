964d130a4c7ac598d1b90b3fcfd4373b9ac844c2
eavichay <eavichay@gmail.com>
2017-11-23 16:14:20 +0200

    fix: dot-net cli now builds tree instead of flat list

--- dev-test.js
+++ dev-test.js
@@ -1,23 +1,11 @@
 var parser = require('./lib');
 var fs = require('fs');
 
-function getFlag(flag) {
-  var index = process.argv.indexOf('--' + flag);
-  if (index === -1) {
-    return null;
-  }
-  return process.argv[index + 1];
-}
-
-var targetFile = getFlag('name');
-
-if (!targetFile) {
-  console.error('No target file specified');
-  process.exit(1);
-}
-
 parser
-  .inspect(null, targetFile, null)
+  .inspect(null, './test/stubs/dummy_project_2/obj/project.assets.json', {
+    packageManager: 'nuget',
+    packagesFolder: './test/stubs/dummy_project_2/_packages',
+  })
   .then(function (result) {
     if (!result) {
       console.log('Error parsing ' + targetFile);
---
+++ test/parse-dotnet-cli.test.js
@@ -0,0 +1,25 @@
+var test = require('tap').test;
+var plugin = require('../lib/index');
+var projectPath = './test/stubs/dummy_project_2/';
+var manifestFile = projectPath + 'obj/project.assets.json';
+var expectedTree = require('./stubs/dummy_project_2/expected.json');
+
+test('parse dotnet-cli project and traverse packages', function (t) {
+  plugin.inspect(
+    projectPath,
+    manifestFile,
+    {
+      packagesFolder: projectPath + './_packages',
+    })
+    .then(function (result) {
+      t.deepEqual(
+        result,
+        expectedTree,
+        'expects project data to be correct'
+      );
+      t.end();
+    })
+    .catch(function (error) {
+      t.fail('Error was thrown: ' + err);
+    });
+});
