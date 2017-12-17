ad22ad0d2cd64016ee76e348110f02b38cd53447
Aryeh Beitz <aryehbeitz@gmail.com>
2017-12-17 14:37:48 +0200

    fix: take major version

---
+++ test/parse-dotnet-cli-2-with_duplicate_deps.test.js
@@ -0,0 +1,22 @@
+var test = require('tap').test;
+var plugin = require('../lib/index');
+var projectPath = './test/stubs/dotnet_2/';
+var manifestFile = projectPath + 'obj/project.assets.json';
+var expectedTree = require('./stubs/dotnet_2/expected.json');
+
+test('parse dotnet-cli 2 project and traverse packages', function (t) {
+  plugin.inspect(
+    projectPath,
+    manifestFile)
+    .then(function (result) {
+      t.deepEqual(
+        result,
+        expectedTree,
+        'expects project data to be correct'
+      );
+      t.end();
+    })
+    .catch(function (error) {
+      t.fail('Error was thrown: ' + err);
+    });
+});
