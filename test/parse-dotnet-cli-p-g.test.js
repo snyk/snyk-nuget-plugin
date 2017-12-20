139feb19c94032de0e85d221b35e48139ea817cb
Aryeh Beitz <aryehbeitz@gmail.com>
2017-12-20 15:19:06 +0200

    fix: take major version (#20)

--- test/parse-dotnet-cli-p-g.test.js
+++ test/parse-dotnet-cli-p-g.test.js
@@ -1,13 +1,16 @@
 var test = require('tap').test;
 var plugin = require('../lib/index');
-var projectPath = './test/stubs/dotnet_project/';
+var projectPath = './test/stubs/dotnet_p_g/';
 var manifestFile = projectPath + 'obj/project.assets.json';
-var expectedTree = require('./stubs/dotnet_project/expected.json');
+var expectedTree = require('./stubs/dotnet_p_g/expected.json');
 
-test('parse dotnet-cli 2 project and traverse packages', function (t) {
+test('parse dotnet-cli project and traverse packages', function (t) {
   plugin.inspect(
-    null,
-    manifestFile)
+    projectPath,
+    manifestFile,
+    {
+      packagesFolder: projectPath + './_packages',
+    })
     .then(function (result) {
       t.deepEqual(
         result,
