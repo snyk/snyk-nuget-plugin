8e4b7a6152b47eb2b69133f21d41b3defc49a067
Anton Drukh <anton@snyk.io>
2018-08-05 21:23:16 +0300

    chore: eslint instead of jscs

--- test/parse-dotnet-cli-2-with_duplicate_deps.test.js
+++ test/parse-dotnet-cli-2-with_duplicate_deps.test.js
@@ -5,19 +5,19 @@ var manifestFile = projectPath + 'obj/project.assets.json';
 var expectedTree = require('./stubs/dotnet_2/expected.json');
 
 test('parse dotnet-cli 2 with duplicate deps project and traverse packages',
-function (t) {
-  plugin.inspect(
-    null,
-    manifestFile)
-    .then(function (result) {
-      t.deepEqual(
-        result,
-        expectedTree,
-        'expects project data to be correct'
-      );
-      t.end();
-    })
-    .catch(function (error) {
-      t.fail('Error was thrown: ' + error);
-    });
-});
+  function (t) {
+    plugin.inspect(
+      null,
+      manifestFile)
+      .then(function (result) {
+        t.deepEqual(
+          result,
+          expectedTree,
+          'expects project data to be correct'
+        );
+        t.end();
+      })
+      .catch(function (error) {
+        t.fail('Error was thrown: ' + error);
+      });
+  });
--- test/parse-dotnet-cli.test.js
+++ test/parse-dotnet-cli.test.js
@@ -2,8 +2,6 @@ var test = require('tap').test;
 var plugin = require('../lib/index');
 var projectPath = './test/stubs/dummy_project_2/';
 var manifestFile = 'obj/project.assets.json';
-var expectedTree = require('./stubs/dummy_project_2/expected.json');
-var fs = require('fs');
 
 test('parse dotnet-cli project without frameworks field', function (t) {
   plugin.inspect(
@@ -12,8 +10,8 @@ test('parse dotnet-cli project without frameworks field', function (t) {
     {
       packagesFolder: projectPath + './_packages',
     }).then(function () {
-      t.fail('Expected an error to be thrown');
-    })
+    t.fail('Expected an error to be thrown');
+  })
     .catch(function (error) {
       t.equals(error.message,
         'No frameworks were found in project.assets.json');
