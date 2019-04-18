4536256251a89495a9156c6018233127fe3504e6
nrodrigues <nrodrigues@coveo.com>
2019-04-18 18:12:19 -0400

    * Modified the way we use --assets-project-name argument + Added tests

---
+++ test/parse-core-no-project-name-in-assets-file.test.js
@@ -0,0 +1,41 @@
+'use strict';
+const test = require('tap').test;
+const plugin = require('../lib/index');
+const projectPath = './test/stubs/coreNoProjectNameInAssets/';
+const manifestFile = 'obj/project.assets.json';
+const expectedTree = require('./stubs/coreNoProjectNameInAssets/expected.json');
+
+test('parse core without project name in project assets file with assets-project-name argument', function (t) {
+  plugin.inspect(
+    projectPath,
+    manifestFile,
+    { 'assets-project-name': true })
+    .then(function (result) {
+      t.deepEqual(
+        result,
+        expectedTree,
+        'expects project data to be correct'
+      );
+      t.end();
+    })
+    .catch(function (error) {
+      t.fail('Error was thrown: ' + error);
+    });
+});
+
+test('parse core without project name in project assets file without assets-project-name argument', function (t) {
+  plugin.inspect(
+    projectPath,
+    manifestFile)
+    .then(function (result) {
+      t.deepEqual(
+        result,
+        expectedTree,
+        'expects project data to be correct'
+      );
+      t.end();
+    })
+    .catch(function (error) {
+      t.fail('Error was thrown: ' + error);
+    });
+});
---
+++ test/parse-core-project-name-in-assets-file.test.js
@@ -0,0 +1,42 @@
+'use strict';
+const test = require('tap').test;
+const plugin = require('../lib/index');
+const projectPath = './test/stubs/coreProjectNameInAssets/';
+const manifestFile = 'obj/project.assets.json';
+const expectedTreeWithAssetsProjectNameArgument = require('./stubs/coreProjectNameInAssets/expectedWithAssetsProjectNameArgument.json');
+const expectedTreeWithoutAssetsProjectNameArgument = require('./stubs/coreProjectNameInAssets/expectedWithoutAssetsProjectNameArgument.json');
+
+test('parse core with project name in project assets file with assets-project-name', function (t) {
+  plugin.inspect(
+    projectPath,
+    manifestFile,
+    { 'assets-project-name': true })
+    .then(function (result) {
+      t.deepEqual(
+        result,
+        expectedTreeWithAssetsProjectNameArgument,
+        'expects project data to be correct'
+      );
+      t.end();
+    })
+    .catch(function (error) {
+      t.fail('Error was thrown: ' + error);
+    });
+});
+
+test('parse core with project name in project assets file without assets-project-name', function (t) {
+  plugin.inspect(
+    projectPath,
+    manifestFile)
+    .then(function (result) {
+      t.deepEqual(
+        result,
+        expectedTreeWithoutAssetsProjectNameArgument,
+        'expects project data to be correct'
+      );
+      t.end();
+    })
+    .catch(function (error) {
+      t.fail('Error was thrown: ' + error);
+    });
+});
