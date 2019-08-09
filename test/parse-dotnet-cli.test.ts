5c523fecd81339b1e1dfa2d7b49b9cef2e79ff23
ivanstanev <istanev93@gmail.com>
2019-08-09 17:00:36 +0100

    test: add tests for getTargetFrameworkFromPackagesConfig

--- test/parse-dotnet-cli.test.ts
+++ test/parse-dotnet-cli.test.ts
@@ -4,6 +4,9 @@ import * as plugin from '../lib/index';
 const projectPath = './test/stubs/dummy_project_2/';
 const manifestFile = 'obj/project.assets.json';
 
+const packagesConfigOnlyPath = './test/stubs/packages-config-only/';
+const packagesConfigOnlyManifestFile = 'packages.config';
+
 test('parse dotnet-cli project without frameworks field', async (t) => {
   try {
     await plugin.inspect(projectPath, manifestFile, {packagesFolder: projectPath + './_packages'});
@@ -11,4 +14,13 @@ test('parse dotnet-cli project without frameworks field', async (t) => {
   } catch(error) {
     t.equals(error.message, 'No frameworks were found in project.assets.json');
   }
+});
+
+test('parse dotnet-cli project with packages.config only', async (t) => {
+  const res = await plugin.inspect(packagesConfigOnlyPath, packagesConfigOnlyManifestFile);
+  t.equal(res.package.name, 'packages-config-only', 'expected packages-config-only name');
+  // expect the first found targetRuntime to be returned by the plugin
+  t.equal(res.plugin.targetRuntime, 'net452', 'expected net452 framework');
+  t.ok(res.package.dependencies.jQuery, 'jQuery should be found because specified');
+  t.ok(res.package.dependencies['Moment.js'], 'Moment.js should be found because specified');
 });
