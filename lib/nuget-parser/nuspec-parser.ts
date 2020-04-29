c04d0f189582dab29af1e73c5118e7d3aa97654c
Liliana Kastilio <lili@snyk.io>
2020-04-29 16:15:22 +0100

    feat: patch vulnerable lodash using @snyk/lodash

--- lib/nuget-parser/nuspec-parser.ts
+++ lib/nuget-parser/nuspec-parser.ts
@@ -3,7 +3,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as parseXML from 'xml2js';
 import * as dependency from './dependency';
-import * as _ from 'lodash';
+import * as _ from '@snyk/lodash';
 import * as debugModule from 'debug';
 const debug = debugModule('snyk');
 
