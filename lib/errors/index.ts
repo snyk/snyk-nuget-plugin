c2faae9fc988054959ee5dd453b045c5a4abee75
Kaspar Lyngsie <kaspar.moss@snyk.io>
2023-07-20 11:12:24 +0300

    feat: adding new error for dotnet cli commands

--- lib/errors/index.ts
+++ lib/errors/index.ts
@@ -1,4 +1,5 @@
 export { BigTreeError } from './big-tree-error';
+export { CliCommandError } from './cli-command-error';
 export { FileNotFoundError } from './file-not-found-error';
 export { FileNotProcessableError } from './file-not-processable-error';
 export { InvalidManifestError } from './invalid-manifest-error';
