077e08bd92729767faec9ece898b5ec8ef2eb4b7
Kaspar Lyngsie <kaspar.moss@snyk.io>
2023-08-01 13:09:17 +0300

    fix: fixed a .. err .. pretty bad bug

--- test/parsers/parse-core-v2.spec.ts
+++ test/parsers/parse-core-v2.spec.ts
@@ -30,27 +30,59 @@ describe('when generating depGraphs and runtime assemblies using the v2 parser',
     },
   );
 
-  it.each([
-    { useRuntimeDependencies: false, expectedVersion: '4.3.0' },
-    { useRuntimeDependencies: true, expectedVersion: '6.0.0' },
-  ])(
-    'correctly generates a depGraph with or without runtime versions when flag is enabled = $useRuntimeDependencies',
-    async ({ useRuntimeDependencies, expectedVersion }) => {
-      const baseline = await nugetParser.buildDepGraphFromFiles(
-        './test/fixtures/dotnetcore/dotnet_6_published',
-        'obj/project.assets.json',
-        ManifestType.DOTNET_CORE,
-        false,
-        useRuntimeDependencies,
-      );
-      expect(baseline.dependencyGraph).toBeDefined();
-      const depGraphBaseline = baseline.dependencyGraph;
-      expect(depGraphBaseline.getPkgs()).toContainEqual({
-        name: 'System.Net.Http',
-        version: expectedVersion,
-      });
-    },
-  );
+  it('correctly generates a depGraph with or without runtime versions ', async () => {
+    // First generate the graph normally without new functionality.
+    let useRuntimeDependencies = false;
+    const baseline = await nugetParser.buildDepGraphFromFiles(
+      './test/fixtures/dotnetcore/dotnet_6_published',
+      'obj/project.assets.json',
+      ManifestType.DOTNET_CORE,
+      false,
+      useRuntimeDependencies,
+    );
+
+    // Then do the same with the new functionality and validate the graph looks the same, only newer versions for runtime dependencies.
+    useRuntimeDependencies = true;
+    const withRuntimeDeps = await nugetParser.buildDepGraphFromFiles(
+      './test/fixtures/dotnetcore/dotnet_6_published',
+      'obj/project.assets.json',
+      ManifestType.DOTNET_CORE,
+      false,
+      useRuntimeDependencies,
+    );
+
+    // Assert that the existing logic shows an older version of a runtime dependency:
+    expect(baseline.dependencyGraph).toBeDefined();
+    expect(baseline.dependencyGraph.getPkgs()).toContainEqual({
+      name: 'System.Net.Http',
+      version: '4.3.0',
+    });
+
+    // Assert that with runtime deps it correctly reflects the net6.0 runtime version:
+    expect(withRuntimeDeps.dependencyGraph).toBeDefined();
+    expect(withRuntimeDeps.dependencyGraph.getPkgs()).toContainEqual({
+      name: 'System.Net.Http',
+      version: '6.0.0',
+    });
+
+    // Assert that no construction of the depGraph otherwise was destroyed in the process,
+    // by looking at a non-runtime dependencies relations to the graph:
+    const pkg = { name: 'Microsoft.NETCore.Targets', version: '1.1.0' };
+
+    // Map the list to ignore versions for easier comparison, as it will otherwise fail with versions being different
+    // between baseline and runtime:
+    const baselinePathsToRoot = baseline.dependencyGraph
+      .pkgPathsToRoot(pkg)
+      .map((inner) => inner.map(({ name }) => ({ name })));
+
+    const withRuntimeDepsPathsToRoot = withRuntimeDeps.dependencyGraph
+      .pkgPathsToRoot(pkg)
+      .map((inner) => inner.map(({ name }) => ({ name })));
+
+    expect(withRuntimeDepsPathsToRoot.length).toEqual(
+      baselinePathsToRoot.length,
+    );
+  });
 
   it('does not allow the runtime beta option to be set on non-dotnet core projects', async () => {
     const projectPath = './test/fixtures/packages-config/repositories-config/';
