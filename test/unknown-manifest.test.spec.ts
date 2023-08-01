ee7151c41f6d1acbfabb0d63fe1d00a4e6f08928
Kaspar Lyngsie <kaspar.moss@snyk.io>
2023-08-01 20:08:40 +0300

    fixup! fix: re-wrote rejects logic and added the .spec ending I forgot about

--- test/unknown-manifest.test.spec.ts
+++ test/unknown-manifest.test.spec.ts
@@ -3,18 +3,14 @@ import * as plugin from '../lib/index';
 
 describe('when parsing with unknown manifests', () => {
   it('Should throw error for unrecognized manifest file', async () => {
-    expect(async () => await plugin.inspect(null, 'unknown.type')).toThrow(
-      'Could not determine manifest type for unknown.type',
-    );
+    await expect(
+      async () => await plugin.inspect(null, 'unknown.type'),
+    ).rejects.toThrow('Could not determine manifest type for unknown.type');
   });
 
   it('Should throw error for unrecognized manifest file', async () => {
-    try {
-      await plugin.inspect(null, 'some.config');
-    } catch (error: unknown) {
-      expect(error.message).toBe(
-        'Could not determine manifest type for some.config',
-      );
-    }
+    await expect(
+      async () => await plugin.inspect(null, 'some.config'),
+    ).rejects.toThrow('Could not determine manifest type for some.config');
   });
 });
