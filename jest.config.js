99ceb85373d7d3e79d56feef189717ad76db506b
Kaspar Lyngsie <kaspar.moss@snyk.io>
2023-08-02 17:11:07 +0300

    chore: tests are still flaky :face_with_thermometer:

--- jest.config.js
+++ jest.config.js
@@ -8,5 +8,5 @@ module.exports = {
   moduleFileExtensions: ['ts', 'js', 'json'],
   forceExit: true,
   // `dotnet` commands on fresh machines requires restoration of modules which can take a while to resolve.
-  testTimeout: 60000,
+  testTimeout: 80000,
 };
