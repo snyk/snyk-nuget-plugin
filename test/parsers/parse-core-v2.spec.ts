fc03472e167ddce2b76c979502058f8ae4a1d818
Kaspar Lyngsie <kaspar.moss@snyk.io>
2023-08-02 16:41:32 +0300

    fix: added another test fixture for netstandard 2.1 and updated depGraph building to support it

--- test/parsers/parse-core-v2.spec.ts
+++ test/parsers/parse-core-v2.spec.ts
@@ -11,6 +11,10 @@ describe('when generating depGraphs and runtime assemblies using the v2 parser',
       description: 'parse dotnet 6.0',
       projectPath: './test/fixtures/dotnetcore/dotnet_6_published',
     },
+    {
+      description: 'parse netstandard 2.1',
+      projectPath: './test/fixtures/dotnetcore/netstandard_21_published',
+    },
   ])(
     'should succeed given a project file and an expected graph for test: $description',
     async ({ projectPath }) => {
