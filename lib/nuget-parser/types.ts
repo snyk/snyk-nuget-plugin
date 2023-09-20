ecc45c6a4be7d4056c1ee580a6efe1e7768e0364
Kaspar Lyngsie <kaspar.moss@snyk.io>
2023-09-20 09:39:22 +0300

    feat: drop framework parser and look directly in assets file

--- lib/nuget-parser/types.ts
+++ lib/nuget-parser/types.ts
@@ -28,6 +28,23 @@ export enum ManifestType {
   PAKET = 'paket',
 }
 
+interface Project {
+  version: string;
+  restore: Record<string, any>;
+  frameworks: Record<string, any>;
+  runtimeIdentifierGraphPath: string;
+}
+
+// .NET core's project.assets.json with the needed fields represented
+export interface ProjectAssets {
+  version: number;
+  targets: Record<string, any>;
+  libraries: Record<string, any>;
+  projectFileDependencyGroups: Record<string, any>;
+  packageFolders: Record<string, any>;
+  project: Project;
+}
+
 // <System.Net.Http, 6.0.0, ...>
 export type AssemblyVersions = Record<string, string>;
 
