a990883b20f98e500bad1d1def4f9e6b4e30a3ee
Kaspar Lyngsie <kaspar.moss@snyk.io>
2023-09-26 10:36:19 +0300

    feat: added a simple memory cache to avoid constant dotnet restores

--- lib/nuget-parser/csharp/generator.ts
+++ lib/nuget-parser/csharp/generator.ts
@@ -3,8 +3,25 @@ import * as fs from 'fs';
 import * as os from 'os';
 import * as types from '../types';
 import * as debugModule from 'debug';
+import * as NodeCache from 'node-cache';
+import * as crypto from 'crypto';
 
 const debug = debugModule('snyk');
+
+const cache = new NodeCache();
+
+// Use a simple, fast and not secure hashing algorithm just to ensure we don't return the same cached location for
+// different types of files. This is mostly to speed up performance of tests, but in theory will also affect customers
+// when scanning for multiple TargetFrameworks.
+function generateCacheKey(files: types.DotNetFile[]): string {
+  const hash = crypto.createHash('sha256');
+  files
+    .map((f) => f.contents)
+    .forEach((content) => {
+      hash.update(content);
+    });
+  return hash.digest('hex');
+}
 
 // Importing .NET code from Typescript is not trivial and a bit lose cannon programming. However, we also want to keep
 // this project dependent on as few packages as possible, so instead of opting into some "run .NET in Typescript" package,
@@ -16,6 +33,12 @@ export function generate(
   tempDirNameSpace: string,
   files: types.DotNetFile[],
 ): string {
+  const key = generateCacheKey(files);
+  const cached: string | undefined = cache.get(key);
+  if (cached) {
+    return cached;
+  }
+
   const tempDir = fs.mkdtempSync(
     path.join(os.tmpdir(), `snyk-nuget-plugin-test-${tempDirNameSpace}-`),
   );
@@ -31,6 +54,8 @@ export function generate(
       .map((f) => f.name)
       .join(',')}) in ${tempDir}`,
   );
+
+  cache.set(key, tempDir);
   return tempDir;
 }
 
