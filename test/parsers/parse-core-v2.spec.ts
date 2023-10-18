1408f9b37f425e1840de7921b27d4636a061e2ad
Kaspar Lyngsie <kaspar.moss@snyk.io>
2023-10-18 18:12:24 +0300

    fix: updated the v2 tests for a single example

--- test/parsers/parse-core-v2.spec.ts
+++ test/parsers/parse-core-v2.spec.ts
@@ -28,7 +28,7 @@ describe('when generating depGraphs and runtime assemblies using the v2 parser',
       targetFramework: undefined,
     },
   ])(
-    'should succeed given a project file and an expected graph for test: $description',
+    'succeeds given a project file and returns a single dependency graph for single-targetFramework projects: $description',
     async ({ projectPath, targetFramework }) => {
       // Run a dotnet restore beforehand, in order to be able to supply a project.assets.json file
       await dotnet.restore(projectPath);
@@ -40,9 +40,11 @@ describe('when generating depGraphs and runtime assemblies using the v2 parser',
         'dotnet-target-framework': targetFramework,
       });
 
-      if (pluginApi.isMultiResult(result)) {
-        throw new Error('received invalid depTree');
+      if (!pluginApi.isMultiResult(result)) {
+        throw new Error('expected a multiResult response from inspection');
       }
+
+      expect(result.scannedProjects.length).toEqual(1);
 
       const expectedGraph = JSON.parse(
         fs.readFileSync(
@@ -50,7 +52,46 @@ describe('when generating depGraphs and runtime assemblies using the v2 parser',
           'utf-8',
         ),
       );
-      expect(result.dependencyGraph?.toJSON()).toEqual(expectedGraph.depGraph);
+      expect(result.scannedProjects[0].depGraph?.toJSON()).toEqual(
+        expectedGraph.depGraph,
+      );
+    },
+  );
+
+  it.each([
+    {
+      description: 'parse dotnet 6.0 and 7.0',
+      projectPath: './test/fixtures/dotnetcore/dotnet_6_and_7',
+      expectedDepGraphs: 2,
+    },
+  ])(
+    'succeeds given a project file and returns multiple dependency graphs for multi-targetFramework projects: $description',
+    async ({ projectPath, expectedDepGraphs }) => {
+      // Run a dotnet restore beforehand, in order to be able to supply a project.assets.json file
+      await dotnet.restore(projectPath);
+
+      const manifestFile = 'obj/project.assets.json';
+
+      const result = await plugin.inspect(projectPath, manifestFile, {
+        'dotnet-runtime-resolution': true,
+      });
+
+      if (!pluginApi.isMultiResult(result)) {
+        throw new Error('expected a multiResult response from inspection');
+      }
+
+      expect(result.scannedProjects.length).toEqual(expectedDepGraphs);
+
+      const expectedGraphs = JSON.parse(
+        fs.readFileSync(
+          path.resolve(projectPath, 'expected_depgraphs.json'),
+          'utf-8',
+        ),
+      );
+
+      result.scannedProjects.forEach((scannedProject, i) => {
+        expect(scannedProject.depGraph?.toJSON()).toEqual(expectedGraphs[i]);
+      });
     },
   );
 
