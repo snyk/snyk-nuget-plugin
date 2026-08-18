8487818ea8e78d4813601a71f65091d3a769205a
Ethan Hutchins <33070066+ethanhutchins33@users.noreply.github.com>
2026-08-18 10:49:48 +0100

    fix: replace `--p` arg syntax with correct `-p` to support legacy .net core 3.1 sdk dotnet restore function

--- lib/nuget-parser/cli/dotnet.ts
+++ lib/nuget-parser/cli/dotnet.ts
@@ -158,7 +158,7 @@ export async function restore(
     '--verbosity',
     'normal',
     projectPath, // No quotes needed - spawn passes arguments directly without shell interpretation
-    '--p=MSBuildEnableWorkloadResolver=true;TreatWarningsAsErrors=false;WarningsAsErrors=',
+    '-p=MSBuildEnableWorkloadResolver=true;TreatWarningsAsErrors=false;WarningsAsErrors=',
   ];
   await handle('restore', command, args, workingDirectory);
   return;
